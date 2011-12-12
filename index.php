7fdda4e4c734973993e8d70537e5a108aeccaac1
Rick Hofstede <rickhofstede@users.noreply.github.com>
2011-12-12 09:11:51 +0000

    Removed /images/legend/legend_demo_lines.png

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 	require_once($nfsenConfig['HTMLDIR']."/conf.php");
 	require_once($nfsenConfig['HTMLDIR']."/nfsenutil.php");
 
-	$version = "v2.2 dev (20111208)";
+	$version = "v2.2 dev (20111212)";
 
 	// Initialize session
 	if (!isset($_SESSION['SURFmap'])) $_SESSION['SURFmap'] = array();
