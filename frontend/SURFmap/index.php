a81d260cc34ce02c50d8bd9bbc0802c35675e395
Rick Hofstede <rickhofstede@users.noreply.github.com>
2012-05-30 06:39:54 +0000

    Updated version numbers to latest release date

--- frontend/SURFmap/index.php
+++ frontend/SURFmap/index.php
@@ -20,7 +20,7 @@
 	require_once($nfsenConfig['HTMLDIR']."/conf.php");
 	require_once($nfsenConfig['HTMLDIR']."/nfsenutil.php");
 
-	$version = "v2.3 dev (20120516)";
+	$version = "v2.3 stable (20120530)";
 
 	// Initialize session
 	if (!isset($_SESSION['SURFmap'])) $_SESSION['SURFmap'] = array();
