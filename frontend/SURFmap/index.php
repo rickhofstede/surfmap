e30bfb6722d1dbcea0927d2c95d85079fe16e6cc
Rick Hofstede <rickhofstede@users.noreply.github.com>
2013-05-11 14:27:00 +0000

    Bumped trunk version number to '3.0b3'

--- frontend/SURFmap/index.php
+++ frontend/SURFmap/index.php
@@ -11,7 +11,7 @@
      require_once("config.php");
      require_once("constants.php");
      
-     $version = "3.0b2 (20130511)";
+     $version = "3.0b3 (20130511)";
 
      // Initialize session
      if (!isset($_SESSION['SURFmap'])) $_SESSION['SURFmap'] = array();
