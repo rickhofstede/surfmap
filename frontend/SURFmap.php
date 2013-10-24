4b5c0a60ba38654fcaddee05f47072f5d68e2161
Rick Hofstede <rickhofstede@users.noreply.github.com>
2013-10-24 14:00:59 +0000

    Merged documentroot fix (SSHCure) to SURFmap

--- frontend/SURFmap.php
+++ frontend/SURFmap.php
@@ -1,18 +1,24 @@
 <?php
-	/*******************************
-	 # backend.php [SURFmap]
-	 # Author: Rick Hofstede <r.j.hofstede@utwente.nl>
-	 # University of Twente, The Netherlands
-	 #
-	 # LICENSE TERMS: 3-clause BSD license (outlined in license.html)
-	 *******************************/
-	
-	function SURFmap_ParseInput( $plugin_id ) {
-		$_SESSION['refresh'] = 0;
-	}
 
-	function SURFmap_Run( $plugin_id ) {
-		echo "<iframe id='surfmapParentIFrame' src='plugins/SURFmap/index.php' frameborder='0' style='width:100%; height:100%'>Your browser does not support iframes.</iframe>";	
-	}
+function SURFmap_ParseInput ($plugin_id) {
+	$_SESSION['refresh'] = 0;
+}
+
+function SURFmap_Run ($plugin_id) {
+    $url = "";
+    if (strpos(getenv('DOCUMENT_ROOT'), 'nfsen') === false) { // DOCUMENT_ROOT does not contain 'nfsen' folder
+        // Add trailing slash when not present yet
+        if (substr(getenv('DOCUMENT_ROOT'), -1) !== "/") {
+            $url .= '/';
+        }
+        
+        $url .= 'nfsen/';
+    }
+    
+    $url .= 'plugins/SURFmap/index.php';
+    
+    
+	echo "<iframe id='surfmapParentIFrame' src='plugins/SURFmap/index.php' frameborder='0' style='width:100%; height:100%'>Your browser does not support iframes.</iframe>";
+}
 
 ?>
