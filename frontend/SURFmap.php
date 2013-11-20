cac0df1520a7b1d4843478edd436d265659edf27
Rick Hofstede <rickhofstede@users.noreply.github.com>
2013-11-20 21:30:26 +0000

    Fixed DOCUMENTROOT problem in SURFmap.php

--- frontend/SURFmap.php
+++ frontend/SURFmap.php
@@ -5,18 +5,7 @@ function SURFmap_ParseInput ($plugin_id) {
 }
 
 function SURFmap_Run ($plugin_id) {
-    $url = "";
-    if (strpos(getenv('DOCUMENT_ROOT'), 'nfsen') === false) { // DOCUMENT_ROOT does not contain 'nfsen' folder
-        // Add trailing slash when not present yet
-        if (substr(getenv('DOCUMENT_ROOT'), -1) !== "/") {
-            $url .= '/';
-        }
-        
-        $url .= 'nfsen/';
-    }
-    
-    $url .= 'plugins/SURFmap/index.php';
-    
+    $url = 'plugins/SURFmap/index.php';
     
 	echo "<iframe id='surfmapParentIFrame' src='{$url}' frameborder='0' style='width:100%; height:100%'>Your browser does not support iframes.</iframe>";
 }
